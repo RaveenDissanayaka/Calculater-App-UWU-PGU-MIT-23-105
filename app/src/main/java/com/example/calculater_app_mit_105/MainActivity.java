4722a6053bd2daa155d40a3bcd867dc3933d48bd
RaveenDissanayaka <raveensalinda50@gmail.com>
2024-08-11 07:36:06 +0530

    initial commit

---
+++ app/src/main/java/com/example/calculater_app_mit_105/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.calculater_app_mit_105;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
